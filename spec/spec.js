a727fc030970c70f35d684588468f1f6392dd4b0
nuk <nuk.anime13@gmail.com>
2012-07-04 17:54:07 -0300

    We can handle multiple lines \o/

--- spec/spec.js
+++ spec/spec.js
@@ -10,7 +10,7 @@ describe("word", function() {
 	});
 	
 	it("finds a word if its all we have", function() {
-		w = new WordSearch("abacateandcarro")
+		w = new WordSearch(["abacateandcarro"])
 		w.add2dictionary("abacate")
 		w.add2dictionary("carro")
 
@@ -20,12 +20,24 @@ describe("word", function() {
 
 
 	it("rejcts when selection is not a word", function() {
-		w = new WordSearch("abacateandcarro")
+		w = new WordSearch(["abacateandcarro"])
 		w.add2dictionary("abacate")
 		w.add2dictionary("carro")
 
 		expect(w.mark([0,0],[0,5])).toBe(false)
 		expect(w.mark([0,11],[0,14])).toBe(false)
 	});
+
+
+	it("another line in the matrix", function() {
+		w = new WordSearch(["abacateandcarro","cafeandsometea"])
+		w.add2dictionary("cafe")
+		w.add2dictionary("tea")
+
+		expect(w.mark([1,0],[1,3])).toBe(true)
+		expect(w.mark([1,11],[1,13])).toBe(true)
+	});
+
+
 	
 });
--- src/word.js
+++ src/word.js
@@ -8,10 +8,12 @@ WordSearch = function(matrix){
 	};
 
 	this.mark = function(start, end){
-		for (var i = 0; i < this.dictionary.length; i++){
-			if (this.inner_matrix.substring(start[1],end[1]+1) == this.dictionary[i])
-				return true;
-		}
+		
+			for (var i = 0; i < this.dictionary.length; i++){
+				if (this.inner_matrix[start[0]].substring(start[1],end[1]+1) == this.dictionary[i])
+					return true;
+			}
+		
 		return false;
 	};
 }
